8859287f20347fd7c4d5b3303c7df4bcae4b971b
Emma Guy <emmag87@gmail.com>
2016-04-10 10:56:15 +0100

    Render single ship

---
+++ core/src/com/colladatest/LoadModelsTest.java
@@ -0,0 +1,79 @@
+package com.colladatest;
+
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.PerspectiveCamera;
+import com.badlogic.gdx.graphics.g3d.Environment;
+import com.badlogic.gdx.graphics.g3d.Model;
+import com.badlogic.gdx.graphics.g3d.ModelBatch;
+import com.badlogic.gdx.graphics.g3d.ModelInstance;
+import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
+import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
+import com.badlogic.gdx.graphics.g3d.utils.CameraInputController;
+import com.badlogic.gdx.utils.Array;
+
+public class LoadModelsTest extends ApplicationAdapter {
+    private static final String SHIP = "ship.g3db";
+
+    private static final String MODEL_PATH = SHIP;
+
+    private final Array<ModelInstance> instances = new Array<ModelInstance>();
+
+    private PerspectiveCamera cam;
+    private CameraInputController camController;
+    private ModelBatch modelBatch;
+    private AssetManager assets;
+    private Environment environment;
+    private boolean loading;
+
+    @Override public void create() {
+        modelBatch = new ModelBatch();
+        environment = new Environment();
+        environment.set(new ColorAttribute(ColorAttribute.AmbientLight, 0.4f, 0.4f, 0.4f, 1f));
+        environment.add(new DirectionalLight().set(0.8f, 0.8f, 0.8f, -1f, -0.8f, -0.2f));
+
+        cam = new PerspectiveCamera(67, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        cam.position.set(3f, 3f, 3f);
+        cam.lookAt(0, 0, 0);
+        cam.near = 1f;
+        cam.far = 300f;
+        cam.update();
+
+        camController = new CameraInputController(cam);
+        Gdx.input.setInputProcessor(camController);
+
+        assets = new AssetManager();
+        assets.load(MODEL_PATH, Model.class);
+        loading = true;
+    }
+
+    private void loadModel() {
+        Model ship = assets.get(MODEL_PATH, Model.class);
+        ModelInstance shipInstance = new ModelInstance(ship);
+        instances.add(shipInstance);
+        loading = false;
+    }
+
+    @Override public void render() {
+        if (loading && assets.update()) {
+            loadModel();
+        }
+        camController.update();
+
+        Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
+
+        modelBatch.begin(cam);
+        modelBatch.render(instances, environment);
+        modelBatch.end();
+    }
+
+    @Override public void dispose() {
+        modelBatch.dispose();
+        instances.clear();
+        assets.dispose();
+    }
+}
